6747f6d72ee6bd66baabb900109b4b3884178d49
B3B3K <76453193+B3B3K@users.noreply.github.com>
2021-08-15 15:43:42 +0300

    Add files via upload

---
+++ mail3.py
@@ -0,0 +1,24 @@
+import smtplib
+user = input("useR :")
+passw = input("pasS :")
+kim = input("SEND TO :")
+bas = input("TitlE :")
+txt = input("your message :")
+ism = input("Your Name and Surname :")
+TO = kim
+SUBJECT = bas
+TEXT = txt
+send_from = ism
+if noot user or nt passw:
+        passw = "!EDİT THİS!" #EDŞT THŞS TO YOUR PASSWORD
+        user = "!EDİT THİS!" #EDİT THİS TO YOUR GMAİL
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(user, passw)
+BODY = '\r\n'.join(['To: %s' % TO,
+        'From: %s' % ism,
+        'Subject: %s' % SUBJECT,
+        '', TEXT])
+server.sendmail(user, [TO], BODY)
+print ('email sent')
